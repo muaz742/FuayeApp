f9084c39ca4e1a8be022e50fbc06e83ebc3e152a
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2025-11-09 11:14:15 +0300

    Remove font check.

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-/** React & React Native */
-import { useState, useEffect } from 'react';
-import { ActivityIndicator, View } from 'react-native';
-
 /** React Navigation */
 import { NavigationContainer } from '@react-navigation/native';
 import MainTab from './navigators/MainTab';
@@ -16,26 +12,6 @@ import { Ionicons } from '@expo/vector-icons';
 
 /** Main Component */
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        ...Ionicons.font,
-      });
-      setFontsLoaded(true);
-    }
-    loadFonts();
-  }, []);
-
-  if (!fontsLoaded) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-
   return (
     <Provider store={store}>
       <NavigationContainer>
